17139644c40af9241eceb5df811a84f6605d0445
Jake Kile <jkile9@gmail.com>
2021-01-21 00:15:41 -0700

    removed purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    enabled: false,
+    content: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  },
   darkMode: 'media', // 'media' or 'class'
   theme: {
     extend: {
